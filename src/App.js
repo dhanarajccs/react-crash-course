812320b7a93be1aa963c7e40ec60da196a2acb75
dhanaraj ccs <dhanarajccs@gmail.com>
2022-04-30 15:24:54 +0530

    Arrow Function - Default Export

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import Greet from './components/Greet';
+// import Greet from './components/Greet';          // Functional Component
+
+import Greet from './components/Greet';          // Functional Component - (Arrow Function)
+import MyComponent from './components/Greet';    //  Default Export - allows us with any name
 
 function App() {
   return (
     <div className="App">
      <Greet />
+     <MyComponent />
     </div>
   );
 }
 
-export default App;
+export default App; 
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,27 @@
+/*
 function Greet() {
     return <h1>Hello Dhanaraj !</h1>
 }
 
 export default Greet 
+*/
+
+// Arrow Function
+
+// Default Export
+
+const Greet = () => {
+    return <h1>Hello Dhanaraj !</h1>
+}
+export default Greet
+
+
+const Greet = () => (
+    <h1>Hello Dhanaraj !</h1>
+)
+export default Greet
+
+
+const Greet = () => <h1>Hello Dhanaraj !</h1>
+export default Greet
+
