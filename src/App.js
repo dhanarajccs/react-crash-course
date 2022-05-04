fc3b2c9fc1ac264864ae06758c4c976f13c416d4
dhanaraj ccs <dhanarajccs@gmail.com>
2022-05-04 13:28:25 +0530

    Styling - CSS Module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import { Inline } from './components/Inline';  // Styling 
+import './appStyles.css'                      // CSS Stylesheet
+import styles from './appStyles.module.css'   // CSS Module
+import { Inline } from './components/Inline';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Inline />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
     </div>
   );
 }
 
 export default App; 
+
--- src/components/Inline.js
+++ src/components/Inline.js
@@ -4,6 +4,7 @@ export const Inline = () => {
 }
 */
 
+/*
 export const Inline = () => {
     const heading = {
         fontSize: '72px',
@@ -13,3 +14,30 @@ export const Inline = () => {
         <div style={heading}>Inline</div>
     )
 }
+*/
+
+export const Inline = () => {
+    const heading = {
+        fontSize: '72px',
+        color: 'blue',
+    }
+    return <h1 style={heading}>Inline</h1>
+}
+
+
+// You can use Regular CSS at every child component as well.
+/*
+export const Inline = () => {
+    return <h1 className='error'>Error</h1>
+}
+*/
+
+// You can’t be used in the child component.
+/*
+export const Inline = () => {
+    return <h1 className={styles.success}>Success</h1>;
+}
+*/
+
+
+
